68f73bdff3bd5a5d973031bfb773b5041b8cb6ba
Hamzah Fayad <hamzah.fayad@glow-berlin.de>
2023-10-17 12:34:17 +0200

    color picker + functionality

--- 001_pixelart/index.js
+++ 001_pixelart/index.js
@@ -7,17 +7,53 @@ window.addEventListener("load", () => {
     pixels.appendChild(pixel);
   }
 
-  //color pixel
+  //variables
+  let currentCol = document.getElementById("colorpicker").value;
+
+  //helper function: color pixel
+  const changeColor = (el, col) => {
+    el.style.background = col;
+  };
+
+  //set color picker value
+  let inputColor = document.getElementById("colorpicker");
+  inputColor.addEventListener("change", (e) => {
+    currentCol = e.target.value;
+    console.log(currentCol);
+  });
+
+  //Event Listeners to color grid pixels
   let pixel = document.querySelectorAll(".pixel");
 
   for (let p = 0; p < pixel.length; p++) {
-    ["click", "mouseover"].forEach((e) => {
-      pixel[p].addEventListener(e, () => {
-        pixel[p].style.background = "red";
-        console.log("x");
-      });
+    pixel[p].addEventListener("click", () => {
+      changeColor(pixel[p], currentCol);
+      pixel[p].classList.add("colored");
+      // if (pixel[p].classList.contains("colored") && currentCol !== "#ffffff") {
+      //   pixel[p].style.pointerEvents = "none";
+      // }
+    });
+    pixel[p].addEventListener("mousedown", () => {
+      changeColor(pixel[p], currentCol);
+    });
+    pixel[p].addEventListener("mouseover", (e) => {
+      if (e.buttons === 1) {
+        //pixel[p].style.background = "red";
+        changeColor(pixel[p], currentCol);
+      }
     });
   }
+
+  // for (let p = 0; p < pixel.length; p++) {
+  //   ["click", "mouseover"].forEach((e) => {
+  //     pixel[p].addEventListener(e, () => {
+  //       if (e.buttons === 1) {
+  //         pixel[p].style.background = "red";
+  //         console.log("x");
+  //       }
+  //     });
+  //   });
+  // }
 });
 
 //columns x rows
